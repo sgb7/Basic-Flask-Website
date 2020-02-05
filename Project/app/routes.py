62187669c53c04091898c3cff3a27fe3553b94bf
Summer G Banister <sgb7@humboldt.edu>
2020-02-05 14:16:46 -0800

    Edited Basic_Flask_Website

--- Project/app/routes.py
+++ Project/app/routes.py
@@ -5,3 +5,10 @@ from app import app
 def home():
     return render_template("home.html")
 
+@app.route('/edit')
+def edit():
+    return render_template("edit.html")
+
+@app.route('/testing')
+def testing():
+    return render_template("experiment.html")
