b976e02e19a4d0b6b44951729d8218f8e4ef2a67
Summer <sgbanister@gmail.com>
2020-02-12 14:36:05 -0800

    Edited Basic_Flask_Website

--- Project/app/routes.py
+++ Project/app/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, flash, redirect, url_for, make_response
 from app import app
 from app.forms import LoginForm
-import os
+from os import listdir
+from os.path import isfile, join
 
 @app.route('/home')
 def home():
@@ -9,7 +10,8 @@ def home():
 
 @app.route('/all')
 def all():
-    pages = os.listdir()
+    #mypath = '/Desktop/Bin/Basic-Flask-Website/Project/app/templates'
+    #pages = [f for f in listdir(mypath) if isfile(join(mypath,f))]
     return render_template("all.html", pages=pages)
     
 @app.route('/login', methods=['GET', 'POST'])
